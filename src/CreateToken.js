a4899eb05ac0699313e5fc9a158e97e7b3ac71c2
SpendBCH <SpendBCH@SpendBCH.io>
2018-08-15 00:29:19 -0700

    Add define token event

--- src/CreateToken.js
+++ src/CreateToken.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
 
 const classStyles = theme => ({
     root: {
@@ -26,7 +26,12 @@ class CreateToken extends Component {
             <Typography variant="headline" component="h3">
                 Create Your Token Now
             </Typography>
-            <Button variant="contained" color="primary" className={classes.button}>
+            <Button 
+                variant="contained" 
+                color="primary" 
+                className={classes.button}
+                onClick={this.props.defineToken}
+            >
                 Create Token
             </Button>
         </Paper>
