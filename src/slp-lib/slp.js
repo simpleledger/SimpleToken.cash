191f1e13ae980c15e65863d83eb6947ae812f53b
SpendBCH <SpendBCH@SpendBCH.io>
2018-08-19 03:12:23 -0700

    Move internal seed/address to constructor. Precalculate fees. Monitor for utxo. Move change in genesis to vout 1. Distribute to all output addresses.

--- src/slp-lib/slp.js
+++ src/slp-lib/slp.js
@@ -1,67 +1,166 @@
 import network from './network'
 import slptokentype1 from './slptokentype1'
+import SlpUtils from './slputils'
 
-let BITBOXCli = require('bitbox-cli/lib/bitbox-cli').default;
-let BITBOX = new BITBOXCli();
+let BITBOXCli = require('bitbox-cli/lib/bitbox-cli').default
+let BITBOX = new BITBOXCli()
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 class Slp {
+    constructor() {
+        let mnemonic = BITBOX.Mnemonic.generate(256)
+        let rootSeed = BITBOX.Mnemonic.toSeed(mnemonic)
+        let masterHDNode = BITBOX.HDNode.fromSeed(rootSeed, 'bitcoincash')
+        let hdNode = BITBOX.HDNode.derivePath(masterHDNode, "m/44'/145'/0'")
+        let node0 = BITBOX.HDNode.derivePath(hdNode, "0/0")
+        this.keyPair = BITBOX.HDNode.toKeyPair(node0)
+        let wif = BITBOX.ECPair.toWIF(this.keyPair)
+        let ecPair = BITBOX.ECPair.fromWIF(wif)
+        this.address = BITBOX.ECPair.toLegacyAddress(ecPair)
+        this.cashAddress = BITBOX.Address.toCashAddress(this.address)
 
-    buildGenesisTx(tokenType, ticker, name, urlOrEmail, decimals, initialQuantity, addressQuantities) {
-        this.genesisOpReturn = slptokentype1.buildGenesisOpReturn(
-            tokenType,
+        // TODO: Save mnemonic to local storage for emergency recovery
+    }
+    buildGenesisOpReturn(ticker, name, urlOrEmail, decimals, batonVout, initialQuantity) {
+        return slptokentype1.buildGenesisOpReturn(
+            0x01,
             ticker,
             name,
             urlOrEmail,
             null,
             decimals,
-            null,
+            batonVout,
             initialQuantity
         )
-
-        this.sendGenesisTx(this.genesisOpReturn, addressQuantities)
     }
 
-    async sendGenesisTx(initOpReturn, genesisTokenAddress) {
+    buildSendOpReturn(tokenIdHex, decimals, outputQtyArray) {
+        return slptokentype1.buildSendOpReturn(
+            0x01,
+            tokenIdHex,
+            decimals,
+            outputQtyArray            
+        )
+    }
+
+    calcFee(batonAddress, outputAddressArray) {
+        let genesisFee = this.calcGenesisFee(batonAddress)
+        let sendFee = this.calcSendFee(outputAddressArray)
+        return genesisFee + sendFee
+    }
+
+    calcGenesisFee(batonAddress) {
+        let outputs = 3
+        let dustOutputs = 1
+
+        if (batonAddress != null) {
+            outputs += 1
+            dustOutputs += 1
+        }
+
+        let fee = BITBOX.BitcoinCash.getByteCount({ P2PKH: 1 }, { P2PKH: outputs })
+        fee += dustOutputs * 546
+
+        return fee
+    }
+
+    calcSendFee(outputAddressArray) {
+        let outputs = 4 + outputAddressArray.length
+
+        let fee = BITBOX.BitcoinCash.getByteCount({ P2PKH: 1 }, { P2PKH: outputs })
+        fee += outputAddressArray.length * 546
+
+        return fee
+    }
+
+    async monitorForPayment(fee, onPaymentCB) {
+        while (true) {
+            try {
+                let utxo = await network.getUtxo(this.address)
+                if (utxo && utxo.satoshis >= fee) {
+                    break
+                }
+            } catch (ex) {
+                console.log(ex)
+            }
+
+            sleep(5000)
+        }
+
+        onPaymentCB()
+    }
+
+    async sendGenesisTx(genesisOpReturn, batonAddress) {
         // TODO: Check for fee too large or send leftover to target address
 
-        let mnemonic = 'suggest cabin brand grief pulse phone vicious fluid wear mouse taxi random armor million dolphin clump width inherit tattoo ticket reward clean farm stamp'
-        let rootSeed = BITBOX.Mnemonic.toSeed(mnemonic)
-        let masterHDNode = BITBOX.HDNode.fromSeed(rootSeed, 'bitcoincash')
-        let hdNode = BITBOX.HDNode.derivePath(masterHDNode, "m/44'/145'/0'")
-        let node0 = BITBOX.HDNode.derivePath(hdNode, "0/0")
-        let keyPair = BITBOX.HDNode.toKeyPair(node0)
-        let wif = BITBOX.ECPair.toWIF(keyPair)
-        let ecPair = BITBOX.ECPair.fromWIF(wif)
-        let address = BITBOX.ECPair.toLegacyAddress(ecPair)
-
-        let utxo = await network.getUtxo(address)
+        let utxo = await network.getUtxo(this.address)
         console.log('utxo: ', utxo)
 
-        let targetAddress = address
-
+        let changeVout = 1
         let transactionBuilder = new BITBOX.TransactionBuilder('bitcoincash')
         transactionBuilder.addInput(utxo.txid, utxo.vout)
 
-        let byteCount = BITBOX.BitcoinCash.getByteCount({ P2PKH: 1 }, { P2PKH: 4 })
+        let fee = this.calcGenesisFee(batonAddress)
+        let satoshisAfterFee = utxo.satoshis - fee + 546
 
-        let satoshisAfterFee = utxo.satoshis - 546- byteCount
+        // Genesis OpReturn
+        transactionBuilder.addOutput(genesisOpReturn, 0)
 
-        transactionBuilder.addOutput(initOpReturn, 0)
+        // Genesis token mint
+        transactionBuilder.addOutput(this.address, satoshisAfterFee)
 
-        // All tokens with dust BCH output
-        transactionBuilder.addOutput(genesisTokenAddress, 546)
-
-        // Change
-        transactionBuilder.addOutput(targetAddress, satoshisAfterFee)
+        // Baton address (optional)
+        if (batonAddress != null) {
+            transactionBuilder.addOutput(batonAddress, 546)
+        }
 
         let redeemScript
-        transactionBuilder.sign(0, keyPair, redeemScript, transactionBuilder.hashTypes.SIGHASH_ALL, utxo.satoshis)
+        transactionBuilder.sign(0, this.keyPair, redeemScript, transactionBuilder.hashTypes.SIGHASH_ALL, utxo.satoshis)
 
         let hex = transactionBuilder.build().toHex()
 
-        network.sendTx(hex)
+        let txid = await network.sendTx(hex)
 
-        return
+        // TODO: Retry sendTx & Calculate txid on tx exists
+        // SlpUtils.txidFromHex(hex)
+
+        // Return change utxo
+        return {
+            txid: txid,
+            vout: changeVout,
+            satoshis: satoshisAfterFee,
+        }
+    }
+
+    async sendSendTx(utxo, sendOpReturn, outputAddressArray, changeAddress) {
+        let transactionBuilder = new BITBOX.TransactionBuilder('bitcoincash')
+        transactionBuilder.addInput(utxo.txid, utxo.vout)
+
+        let fee = this.calcSendFee(outputAddressArray)
+        let satoshisAfterFee = utxo.satoshis - fee
+
+        // Genesis OpReturn
+        transactionBuilder.addOutput(sendOpReturn, 0)
+
+        // Token distribution outputs
+        outputAddressArray.forEach((outputAddress) => {
+            transactionBuilder.addOutput(outputAddress, 546)
+        })
+
+        // Change
+        if (satoshisAfterFee >= 546) {
+            transactionBuilder.addOutput(changeAddress, satoshisAfterFee)
+        }
+
+        let redeemScript
+        transactionBuilder.sign(0, this.keyPair, redeemScript, transactionBuilder.hashTypes.SIGHASH_ALL, utxo.satoshis)
+
+        let hex = transactionBuilder.build().toHex()
+
+        let txid = await network.sendTx(hex)
+
+        return txid
     }
     
 }
