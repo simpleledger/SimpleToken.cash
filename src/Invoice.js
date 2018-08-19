42d1caed60799cf78d07721f9917d6441b72f3c1
SpendBCH <SpendBCH@SpendBCH.io>
2018-08-19 03:05:52 -0700

    Invoice: Show batonAddress, full distribution, supply type. Update paymentAddress/QR and fee

--- src/Invoice.js
+++ src/Invoice.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
-import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
 import QRCode from 'qrcode.react'
 
@@ -17,33 +16,45 @@ class Invoice extends Component {
   render() {
     const { classes } = this.props
 
+    let batonAddress = ""
+    if (!this.props.isFixedSupply) {
+        batonAddress = <div>
+            <b>Baton Address</b> {this.props.batonAddress} <br/>
+        </div>;
+    }
+
+    let initialDistribution = this.props.addressQuantities.map((aq, index) => {
+        return <div key={[index, ...aq]}>
+            <b>{aq.address}</b> {aq.quantity}
+        </div>;
+    })
+
     return (
       <div className={classes.root}>
-        <Paper className={classes.root} elevation={1}>
-            <Typography variant="headline" component="h3">
-                Review
-            </Typography>
-            <Typography component="p">
-                <b>Name</b> {this.props.name} <br/>
-                <b>Ticker</b> {this.props.ticker} <br/>
-                <b>URL</b> {this.props.urlOrEmail} <br/>
-                <b>Decimal Places</b> {this.props.decimalPlaces} <br/>
-                <b>Initial Quantity</b> {this.props.initialQuantity} <br/>
-                <b>Initial Distribution</b> {this.props.slpAddress} <br/>
-            </Typography> <br/>
-            <Typography variant="title">
-                Pay Mining Fee
-            </Typography>
-            <Typography component="p">
-                Send 0.00005000 BCH to bitcoincash:address
-            </Typography>
-            
-            <QRCode value="bitcoincash:address" />
+        <Typography variant="headline" component="h3">
+            Review
+        </Typography>
+        <div>
+            <b>Name</b> {this.props.name} <br/>
+            <b>Ticker</b> {this.props.ticker} <br/>
+            <b>URL</b> {this.props.urlOrEmail} <br/>
+            <b>Decimal Places</b> {this.props.decimalPlaces} <br/>
+            <b>Initial Quantity</b> {this.props.initialQuantity} <br/>
+            <b>Fixed Supply</b> { this.props.isFixedSupply ? "Yes" : "No" } <br/>
+            {batonAddress}
+            <b>Initial Distribution</b> {initialDistribution} <br/>
+        </div> <br/>
+        <Typography variant="title">
+            Pay Mining Fee
+        </Typography>
+        <div>
+            Send {this.props.fee} satoshis to {this.props.paymentAddress}
+        </div>
+        
+        <QRCode value={this.props.paymentAddress} />
 
-            <br/>
-            Waiting for payment... <br/>
-            Payment found. Minting tokens... <br/>
-        </Paper>
+        <br/>
+        Waiting for payment... <br/>
       </div>
     );
   }
